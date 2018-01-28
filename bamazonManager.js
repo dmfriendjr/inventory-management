86ae19695638448ef072d06f4d7c51b5537d8e77
dmfriendjr <davidmfriendjr@gmail.com>
2018-01-28 12:28:00 -0500

    Implemented a manager interface in order to add new products, update quantites, view all products, and view low stock items

---
+++ bamazonManager.js
@@ -0,0 +1,76 @@
+const Products = require('./products');
+const inquirer = require('inquirer');
+
+let productManager = new Products();
+
+function displayUI () {
+    inquirer.prompt({
+        type: 'list',
+        message: 'Choose An Action',
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+        name: 'action' 
+    }).then( async function (result) {
+        switch(result.action) {
+            case 'View Products for Sale':
+                await productManager.getAllProducts();
+                promptForNewAction();
+            break;
+            case 'View Low Inventory':
+                await productManager.getLowQuantityItems();
+                promptForNewAction();
+            break;
+            case 'Add to Inventory':
+            break;
+            case 'Add New Product':
+                addProduct();
+            break;
+        }
+    })
+
+    return;
+}
+
+function promptForNewAction() {
+    inquirer.prompt([
+        {
+        type: 'confirm',
+        message: 'Perform another action?',
+        name: 'confirm'
+        }
+    ]).then ( res => {
+        if (res.confirm) {
+            displayUI();
+        }
+    })
+}
+
+function addProduct() {
+    inquirer.prompt([
+        {
+        type: 'input',
+        message: 'Enter Product Name',
+        name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Enter Department Name',
+            name: 'department'
+        },
+        {
+            type: 'input',
+            message: 'Enter Product Price',
+            name: 'price'
+        },
+        {
+            type: 'input',
+            message: 'Enter Product Quantity',
+            name: 'quantity'
+        }
+    ]).then( product => {
+        productManager.addProduct(product.name, product.department, product.price, product.quantity);
+        console.log('Product Added!');
+        promptForNewAction();
+    })
+}
+
+displayUI();
