31226418eae25032711c99d015f70928fca5d020
dmfriendjr <davidmfriendjr@gmail.com>
2018-01-28 12:18:38 -0500

    Abstracted product-database connection into a module to be required as needed in each segment of application

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,82 +1,7 @@
-const mysql = require('mysql');
+const Products = require('./products');
 const inquirer = require('inquirer');
 
-let isConnected = false;
-let connection;
-
-function makeConnection() {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'bamazon'
-    });
-
-    connection.connect();
-    isConnected = true;
-}
-
-function disconnect() {
-    connection.end();
-    isConnected = false;
-}
-
-function getAllProducts() {
-    if (!isConnected) {
-        makeConnection();
-    }
-
-    connection.query('SELECT * FROM products', (err, res) => {
-        logProductData(res);
-        displayUI();
-    });
-}
-
-function getProdutInfo(productId) {
-    if (!isConnected) {
-        makeConnection();
-    }
-
-    return new Promise( (resolve, reject) => {
-        connection.query('SELECT  * FROM products WHERE item_id = ?', productId, (err, res) => {
-            if (err) reject(err);
-            resolve(res[0]);
-        });
-    })
-}
-
-function checkItemQuantity(productId) {
-    if (!isConnected) {
-        makeConnection();
-    }
-    let quantity;    
-    return new Promise( (resolve, reject) => {
-        connection.query('SELECT * FROM products WHERE item_id = ?', productId, (err, res) => {
-            if (err) throw err;
-            quantity = res[0].stock_quantity;
-            resolve(res[0].stock_quantity);
-        });
-    });
-}
-
-function logProductData(data) {
-    data.forEach(product => {
-        let log = '';
-        for (let attr in product) {
-            log += `${(attr.charAt(0).toUpperCase() + attr.slice(1)).replace(/_/g, " ")}: ${product[attr]} | `
-        }
-        console.log(log);
-    });
-}
-
-function updateProduct(productId, quantity) {
-    return new Promise( (resolve, reject) => {
-        connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [quantity, productId], (err, res) => {
-            if (err) reject(err);
-            resolve(true);
-        });
-    });
-}
+let productManager = new Products();
 
 function displayUI () {
     inquirer.prompt([
@@ -87,6 +12,7 @@ function displayUI () {
         }
     ]).then(productInput => {
         console.log(productInput.productId);
+        console.log('Doing quantity input');
         inquirer.prompt([
             {
                 type: 'input',
@@ -94,13 +20,26 @@ function displayUI () {
                 name: 'productQuantity'
             }
         ]).then(async function(quantityInput) {
-            let productInfo = await getProdutInfo(productInput.productId);
-            getProdutInfo(productInput.productId);
+            let productInfo = await productManager.getProdutInfo(productInput.productId);
+            productManager.getProdutInfo(productInput.productId);
 
             if (quantityInput.productQuantity <= productInfo.stock_quantity) {
-                var orderComplete = await updateProduct(productInput.productId, productInfo.stock_quantity - quantityInput.productQuantity);
+                var orderComplete = await productManager.updateProduct(productInput.productId, productInfo.stock_quantity - quantityInput.productQuantity);
                 if (orderComplete) {
-                    console.log(`Order Complete: $${productInfo.price * quantityInput.productQuantity} Total`)
+                    console.log(`Order Complete: $${(productInfo.price * quantityInput.productQuantity).toFixed(2)} Total`)
+                    inquirer.prompt([
+                        {
+                            type: 'confirm',
+                            message: 'Place another order?',
+                            name: 'confirm'
+                        }
+                    ]).then( res => {
+                        if (res.confirm) {
+                            init();
+                        } else {
+                            return;
+                        }
+                    } )
                 }
             } else {
                 console.log('Insufficient quantity, sorry!');
@@ -109,5 +48,9 @@ function displayUI () {
     })
 }
 
-makeConnection();
-getAllProducts();
+async function init() {
+    await productManager.getAllProducts();
+    displayUI();
+}
+
+init();
---
+++ products.js
@@ -0,0 +1,110 @@
+const mysql = require('mysql');
+
+module.exports = class Products {
+    constructor() {
+        this.makeConnection();
+    }
+
+    makeConnection() {
+        this.connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'bamazon'
+        });
+    
+        this.connection.connect();
+        this.isConnected = true;
+    }
+    
+    disconnect() {
+        this.connection.end();
+        this.isConnected = false;
+    }
+    
+    getAllProducts() {
+        if (!this.isConnected) {
+            this.makeConnection();
+        }
+    
+        return new Promise( (resolve, reject) => {
+            this.connection.query('SELECT * FROM products', (err, res) => {
+                if (err) reject(err);
+                this.logProductData(res);
+                resolve();
+            });
+        })
+    }
+    
+    getProdutInfo(productId) {
+        if (!this.isConnected) {
+            this.makeConnection();
+        }
+    
+        return new Promise( (resolve, reject) => {
+            this.connection.query('SELECT  * FROM products WHERE item_id = ?', productId, (err, res) => {
+                if (err) reject(err);
+                resolve(res[0]);
+            });
+        })
+    }
+    
+    checkItemQuantity(productId) {
+        if (!this.isConnected) {
+            this.makeConnection();
+        }
+        return new Promise( (resolve, reject) => {
+            this.connection.query('SELECT * FROM products WHERE item_id = ?', productId, (err, res) => {
+                if (err) throw err;
+                resolve(res[0].stock_quantity);
+            });
+        });
+    }
+
+    getLowQuantityItems() {
+        if (!this.isConnected) {
+            this.makeConnection();
+        }
+
+        return new Promise( (resolve, reject) => {
+            this.connection.query('SELECT * FROM products WHERE stock_quantity <= 5', (err, res) => {
+                if (err) reject(err);
+                this.logProductData(res);
+                resolve();
+            })
+        })
+    }
+
+    addProduct(productName, departmentName, price, currentStock) {
+        if (!this.isConnected) {
+            this.makeConnection();
+        }
+
+        this.connection.query('INSERT INTO products (product_name, department_name, price, stock_quantity) VALUE (?, ?, ?, ?)', 
+            [productName, departmentName, price, currentStock], (err, res) => {
+                if (err) throw err;
+            });
+    }
+
+    updateProduct(productId, quantity) {
+        return new Promise( (resolve, reject) => {
+            this.connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [quantity, productId], (err, res) => {
+                if (err) reject(err);
+                resolve(true);
+            });
+        });
+    } 
+
+    logProductData(data) {
+        data.forEach(product => {
+            let log = '';
+            for (let attr in product) {
+                log += `${(attr.charAt(0).toUpperCase() + attr.slice(1)).replace(/_/g, " ")}: ${product[attr]} | `
+            }
+            console.log(log);
+        });
+    }
+    
+    
+}
+
